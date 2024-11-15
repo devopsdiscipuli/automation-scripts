18bd8a2791bce6de46a445c0fa4ac3aa758d16dc
devopsdiscipuli <devopsdiscipuli@gmail.com>
2024-11-15 13:15:37 +0530

    Create jenkins-install.sh

---
+++ debian-12/jenkins-install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y
+sudo apt install openjdk-17-jdk -y
+sudo apt install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
